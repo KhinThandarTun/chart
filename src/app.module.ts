7b8a43d3e833e6cd04c1e178b66a7ffb487ac999
Khin Thandar Tun <106916866+KhinThandarTun@users.noreply.github.com>
2024-06-23 21:43:04 +0630

    Learning Chart.js

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClient } from '@angular/common/http';
+import { AppComponent } from './app/app.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Chart} from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,31 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'chart';
+
+  constructor(){
+  
+  }
+
+  ngOnInit(){
+    new Chart("myChart", {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
+
+
